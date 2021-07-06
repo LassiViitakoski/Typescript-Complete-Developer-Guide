7e5ce71d9632b52928f6f6fb0a82c49c8175ea0b
LassiViitakoski <lassi.viitakoski@gmail.com>
2021-07-06 22:21:44 +0300

    Part 117 - Applying a Type to a Generic Class - done

---
+++ Untitled-1.ts
@@ -0,0 +1,14 @@
+class HoldAnything<T> {
+    data: T;
+
+    add(a: T): T {
+        return a;
+    }
+}
+
+const holdNumber = new HoldAnything<number>();
+holdNumber.data = 123;
+holdNumber.add(10);
+
+const holdString = new HoldAnything<string>();
+holdString.data = 'Lassi'
